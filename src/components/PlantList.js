fe696ba27954d95a066923a1d420ef900053094e
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-11 19:00:12 +0300

    correctly display list from db, need to update css

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,19 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants}) {
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <div className="cards">
+      {/* render PlantCards components in here */}
+      {plants.map((plant)=> (
+        <PlantCard key={plant.id} plant={plant}/>
+      )
+
+      )}
+      
+
+      </div>
   );
 }
 
